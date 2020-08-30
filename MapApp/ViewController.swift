b2d40aea5e8d1441ff6f48be07aa7c074abd9ae8
kirill <kirill.titov@outlook.com>
2020-08-30 13:36:24 +0300

    MappApp initiated

---
+++ MapApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapApp
+//
+//  Created by Кирилл Титов on 30.08.2020.
+//  Copyright © 2020 Кирилл Титов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
