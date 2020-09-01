283d785eff9ac7dec3d52037217743266e01d5d0
kirill <kirill.titov@outlook.com>
2020-09-01 17:31:56 +0300

    Interim commit

--- MapApp/App/BusinessLogic/Initial/Interactor/InitialInteractor.swift
+++ MapApp/App/BusinessLogic/Initial/Interactor/InitialInteractor.swift
@@ -5,9 +5,18 @@
 //  Created by kirill on 01/09/2020.
 //  Copyright © 2020 nil. All rights reserved.
 //
+import CoreLocation
 
-class InitialInteractor: InitialInteractorInput {
+class InitialInteractor: NSObject, InitialInteractorInput {
 
     weak var output: InitialInteractorOutput!
 
+	var locationManager: CLLocationManager?
+}
+
+// MARK: - LOCATION MANAGER
+extension InitialInteractor: CLLocationManagerDelegate {
+	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+
+	}
 }
--- MapApp/App/BusinessLogic/Initial/View/InitialViewController.swift
+++ MapApp/App/BusinessLogic/Initial/View/InitialViewController.swift
@@ -7,14 +7,21 @@
 //
 
 import UIKit
+import CoreLocation
+import GoogleMaps
 
-class InitialViewController: UIViewController, InitialViewInput {
+class InitialViewController: UIViewController, InitialViewInput, showAlert {
 
     var output: InitialViewOutput!
+
+//	var locationManager: CLLocationManager?
+
+	@IBOutlet weak var mapView: GMSMapView!
 
     // MARK: Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+
         output.viewIsReady()
     }
 
@@ -23,13 +30,29 @@ class InitialViewController: UIViewController, InitialViewInput {
     }
 }
 
+// MARK: - GOOGLE MAPS
+extension InitialViewController {
+
+	public func addMarker(toCoordinate coordinate: CLLocationCoordinate2D) {
+		let marker = GMSMarker(position: coordinate)
+		marker.map = mapView
+	}
+}
+
+// MARK: - CORE LOCATION
 extension InitialViewController: CLLocationManagerDelegate {
 
 	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-
+		guard let updateCoordinate = locations.first?.coordinate else { return }
+		updateCameraPosition(withLocation: updateCoordinate)
+		addMarker(toCoordinate: updateCoordinate)
 	}
 
 	func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
 		showAlert(message: "\(error)")
+	}
+
+	public func updateCameraPosition(withLocation loction: CLLocationCoordinate2D) {
+		mapView.animate(toLocation: loction)
 	}
 }
